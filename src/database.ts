adacd1954b8f25815c3abfc96480350fe8397427
= <mohamedaziz.benghorbel@insat.ucar.tn>
2024-04-28 09:47:55 +0100

    schema update

--- src/database.ts
+++ src/database.ts
@@ -33,7 +33,18 @@ const skills: Skill[] = [
 ];
 
 
-// Données fictives pour les CVs
+// Données fictives pour les CVs imbriquées
+// const cvs: CV[] = [
+//     { id: 1, name: 'CV Sara', age: 21, job: 'Software Engineer', user: users[0], skills: [skills[0], skills[2], skills[4]] },
+//     { id: 5, name: 'CV Sara2', age: 22, job: 'Software Engineer', user: users[0], skills: [skills[0], skills[2], skills[4], skills[6], skills[8]] },
+//     { id: 2, name: 'CV Rim', age: 21, job: 'Data Scientist', user: users[1], skills: [skills[1], skills[3], skills[5]] },
+//     { id: 3, name: 'CV Ines', age: 21, job: 'Baker', user: users[2], skills: [skills[6], skills[8], skills[10]] },
+//     { id: 4, name: 'CV Aziz', age: 22, job: 'Architect', user: users[3], skills: [skills[7], skills[9], skills[11]] },
+//     { id: 6, name: 'CV Zouaghi', age: 23, job: 'Engineer', user: users[4], skills: [skills[7], skills[9], skills[11], skills[13], skills[15]] },
+// ];
+
+
+// Données fictives pour les CVs non imbriquées
 const cvs: CV[] = [
     { id: 1, name: 'CV Sara', age: 21, job: 'Software Engineer', user: users[0], skills: [skills[0], skills[2], skills[4]] },
     { id: 5, name: 'CV Sara2', age: 22, job: 'Software Engineer', user: users[0], skills: [skills[0], skills[2], skills[4], skills[6], skills[8]] },
@@ -42,7 +53,6 @@ const cvs: CV[] = [
     { id: 4, name: 'CV Aziz', age: 22, job: 'Architect', user: users[3], skills: [skills[7], skills[9], skills[11]] },
     { id: 6, name: 'CV Zouaghi', age: 23, job: 'Engineer', user: users[4], skills: [skills[7], skills[9], skills[11], skills[13], skills[15]] },
 ];
-
 
 
 export const db = {
